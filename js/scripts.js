6d3e3a20de5a58092aebd564e3ea34e07594b750
Ethan Firpo <efirpo@gmail.com>
2020-03-30 11:12:00 -0700

    add array to functionality
    coauthored by Christopher Huber <chuber42.work@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,26 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val().toUpperCase();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
-    console.log();
+  //  var person1Input = $("input#person1").val();
+  //  var person2Input = $("input#person2").val();
+  //  var animalInput = $("input#animal").val();
+  //  var exclamationInput = $("input#exclamation").val().toUpperCase();
+  //  var verbInput = $("input#verb").val();
+  //  var nounInput = $("input#noun").val();
+  //  console.log();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    var responses = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
+    responses.forEach(function(response) {
+      var userInput = $("input#" + response).val();
+      $("." + response).text(userInput);
+    });
+
+//    $(".person1").text(person1Input);
+//    $(".person2").text(person2Input);
+//    $(".animal").text(animalInput);
+//    $(".exclamation").text(exclamationInput);
+//    $(".verb").text(verbInput);
+//    $(".noun").text(nounInput);
     $("#story").show();
 
     event.preventDefault();
